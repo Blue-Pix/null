96a9470b9debda17fa843176845e8897dec2ca38
sakuraya <bluepixel1214@gmail.com>
2020-08-26 23:59:22 +0900

    impl paging

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -15,18 +15,35 @@ error_chain! {
 
 #[derive(Deserialize, Debug)]
 pub struct Tweet {
-  id_str: String,
+  pub id: u64,
   created_at: String,
   text: String,
   retweeted_status: Box<Option<Tweet>>,
+  // user: User,
 }
 
+// #[derive(Deserialize, Debug)]
+// pub struct User {
+//   id_str: String,
+//   name: String,
+//   screen_name: String,
+// }
+
 #[tokio::main]
-pub async fn get_tweets(screen_name: &str) -> Result<Vec<Tweet>> {
-  let url = Url::parse_with_params(
-    "https://api.twitter.com/1.1/statuses/user_timeline.json",
-    &[("screen_name", screen_name), ("include_rts", "false")],
-  )?;
+pub async fn get_tweets<'a>(screen_name: &str, max_id: Option<u64>) -> Result<Vec<Tweet>> {
+  let mut params = vec![
+    ("screen_name", screen_name), 
+    ("include_rts", "false"), 
+    ("trim_user", "true"),
+  ];
+  let id;
+
+  if let Some(max_id) = max_id {
+    id = max_id.to_string();
+    params.push(("max_id", &id));
+  }
+
+  let url = Url::parse_with_params("https://api.twitter.com/1.1/statuses/user_timeline.json", params)?;
   let client = Client::new().get(url).header("authorization", format!("Bearer {}", env::var("TOKEN")?));
   let tweets: Vec<Tweet> = client.send().await?.json().await?;
   Ok(tweets)
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,6 +4,15 @@ use null::api;
 
 fn main() {
     let screen_name = env::var("NAME").expect("Please specify screen_name whose tweets to get.");
-    let tweets = api::get_tweets(&screen_name).unwrap();
-    println!("{:#?}", tweets);
+    let mut max_id = None;
+    for _ in 0..3 {
+        let tweets = api::get_tweets(&screen_name, max_id).unwrap();
+        if tweets.len() == 0 {
+            break;
+        }
+        println!("{:#?}", tweets);
+        println!("=========================================");
+        // max_id = tweets.last().map_or(None, |t| Some(t.id-1));
+        max_id = Some(tweets.last().unwrap().id-1);
+    }
 }
