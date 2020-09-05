35bd8bad359680e2ea297a873c493bc82203111e
sakuraya <bluepixel1214@gmail.com>
2020-09-05 16:27:48 +0900

    impl bulk delete

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,29 +1,51 @@
 extern crate null;
 use null::config::Config;
+use std::fs;
+use std::fs::File;
+use std::io::{Write};
+
+fn main() {
+    let config = Config::new(None).unwrap();
+    fetch_id(&config);
+    // bulk_delete(&config);
+}
 
 #[tokio::main]
-async fn main() {
-    let config = Config::new(None).unwrap();
-    
-    // let mut max_id = None;
-    // for _ in 0..3 {
-    //     let tweets = null::api::get_tweets(&config, max_id).unwrap();
-    //     if tweets.len() == 0 {
-    //         break;
-    //     }
-    //     println!("{:#?}", tweets);
-    //     println!("=========================================");
-    //     // max_id = tweets.last().map_or(None, |t| Some(t.id-1));
-    //     max_id = Some(tweets.last().unwrap().id-1);
-    // }
-    
-    let tweets = null::api::get_tweets(&config, None).await.unwrap();
-    let id = tweets.first().unwrap().id;
-    match null::api::delete_tweet(&config, id).await {
-        Ok(t) => println!("Following tweet is deleted: {}", t.text),
-        Err(e) => eprintln!("Failed to delete tweet: {}", e),
+async fn fetch_id(config: &Config) {
+    let mut max_id = None;
+    let mut file = File::create("id.txt").unwrap();
+    loop {
+        match null::api::get_tweets(&config, max_id).await {
+            Ok(tweets) => {
+                if tweets.len() == 0 {
+                    break;
+                }
+                let ids = tweets.iter().map(|tweet| tweet.id.to_string()).collect::<Vec<_>>().join(",");
+                write!(file, "{},", ids).unwrap();
+                max_id = Some(tweets.last().unwrap().id-1);
+            },
+            Err(e) => {
+                eprintln!("{}", e);
+                break;
+            }
+        }
     }
+}
 
-    // let token = null::oauth::get_request_token();
-    // println!("{:?}", token);
+#[tokio::main]
+async fn bulk_delete(config: &Config) {
+    let ids = fs::read_to_string("id.txt").unwrap();
+    let mut ids = ids.split(',').map(|id| id.parse::<u64>().unwrap()).collect::<Vec<_>>();
+    ids.sort();
+    for n in 0..1000 {
+        match null::api::delete_tweet(&config, ids[n]).await {
+            Ok(tweet) => {
+                println!("{} deleted.", tweet.id);
+            },
+            Err(e) => {
+                eprintln!("{}", e);
+                break;
+            }
+        }
+    };
 }
