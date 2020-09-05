f7ff7426a886e4139ec29339d989a9167263fb08
sakuraya <bluepixel1214@gmail.com>
2020-09-05 15:05:18 +0900

    impl delete api

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,15 +1,17 @@
 use anyhow::Result;
 use reqwest::Client;
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
 use serde::Deserialize;
 use url::Url;
 
 use super::config::{Config};
+use super::oauth;
 
 #[derive(Deserialize, Debug)]
 pub struct Tweet {
   pub id: u64,
   created_at: String,
-  text: String,
+  pub text: String,
   retweeted_status: Box<Option<Tweet>>,
   // user: User,
 }
@@ -21,7 +23,6 @@ pub struct Tweet {
 //   screen_name: String,
 // }
 
-#[tokio::main]
 pub async fn get_tweets(config: &Config, max_id: Option<u64>) -> Result<Vec<Tweet>> {
   let mut params = vec![
     ("include_rts", "false"), 
@@ -39,4 +40,17 @@ pub async fn get_tweets(config: &Config, max_id: Option<u64>) -> Result<Vec<Twee
   let client = Client::new().get(url).header("authorization", format!("Bearer {}", config.token));
   let tweets: Vec<Tweet> = client.send().await?.json().await?;
   Ok(tweets)
+}
+
+pub async fn delete_tweet(config: &Config, id: u64) -> Result<Tweet> {
+  let url = format!("https://api.twitter.com/1.1/statuses/destroy/{}.json", id.to_string());
+  let header = oauth::create_oauth1_header(&url);
+  let mut headers = HeaderMap::new();
+  headers.insert(AUTHORIZATION, header.parse().unwrap());
+  headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/x-www-form-urlencoded"));
+
+  let url = Url::parse(&url)?;
+  let client = Client::new().post(url).headers(headers);
+  let tweet: Tweet = client.send().await?.json().await?;
+  Ok(tweet)
 }
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,29 @@
 extern crate null;
 use null::config::Config;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let config = Config::new(None).unwrap();
-    let mut max_id = None;
-    for _ in 0..3 {
-        let tweets = null::api::get_tweets(&config, max_id).unwrap();
-        if tweets.len() == 0 {
-            break;
-        }
-        println!("{:#?}", tweets);
-        println!("=========================================");
-        // max_id = tweets.last().map_or(None, |t| Some(t.id-1));
-        max_id = Some(tweets.last().unwrap().id-1);
+    
+    // let mut max_id = None;
+    // for _ in 0..3 {
+    //     let tweets = null::api::get_tweets(&config, max_id).unwrap();
+    //     if tweets.len() == 0 {
+    //         break;
+    //     }
+    //     println!("{:#?}", tweets);
+    //     println!("=========================================");
+    //     // max_id = tweets.last().map_or(None, |t| Some(t.id-1));
+    //     max_id = Some(tweets.last().unwrap().id-1);
+    // }
+    
+    let tweets = null::api::get_tweets(&config, None).await.unwrap();
+    let id = tweets.first().unwrap().id;
+    match null::api::delete_tweet(&config, id).await {
+        Ok(t) => println!("Following tweet is deleted: {}", t.text),
+        Err(e) => eprintln!("Failed to delete tweet: {}", e),
     }
+
+    // let token = null::oauth::get_request_token();
+    // println!("{:?}", token);
 }
