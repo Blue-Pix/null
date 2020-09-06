a6ee4e55b846ecb394376e2a37162b9cf3578ec7
sakuraya <bluepixel1214@gmail.com>
2020-09-06 18:45:13 +0900

    impl some tasks

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,21 +1,23 @@
 extern crate null;
+use null::api;
 use null::config::Config;
 use std::fs;
 use std::fs::File;
 use std::io::{Write};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let config = Config::new(None).unwrap();
-    fetch_id(&config);
+    // fetch_id(&config);
     // bulk_delete(&config);
+    // bulk_unretweet(&config);
 }
 
-#[tokio::main]
 async fn fetch_id(config: &Config) {
     let mut max_id = None;
     let mut file = File::create("id.txt").unwrap();
     loop {
-        match null::api::get_tweets(&config, max_id).await {
+        match api::get_my_tweets(&config, max_id).await {
             Ok(tweets) => {
                 if tweets.len() == 0 {
                     break;
@@ -32,13 +34,12 @@ async fn fetch_id(config: &Config) {
     }
 }
 
-#[tokio::main]
 async fn bulk_delete(config: &Config) {
     let ids = fs::read_to_string("id.txt").unwrap();
     let mut ids = ids.split(',').map(|id| id.parse::<u64>().unwrap()).collect::<Vec<_>>();
     ids.sort();
     for n in 0..1000 {
-        match null::api::delete_tweet(&config, ids[n]).await {
+        match api::delete_tweet(&config, ids[n]).await {
             Ok(tweet) => {
                 println!("{} deleted.", tweet.id);
             },
@@ -48,4 +49,25 @@ async fn bulk_delete(config: &Config) {
             }
         }
     };
+}
+
+async fn bulk_unretweet(config: &Config) {
+    match api::get_retweeted_tweets(&config, None).await {
+        Ok(tweets) => {
+            for n in 0..tweets.len() {
+                match api::unretweet(&config, tweets[n].id).await {
+                    Ok(tweet) => {
+                        println!("{} unretweeted.", tweet.text);
+                    },
+                    Err(e) => {
+                        eprintln!("{:#?}", e);
+                        break;
+                    }
+                }
+            };
+        },
+        Err(e) => {
+            eprintln!("{}", e);
+        }
+    }
 }
